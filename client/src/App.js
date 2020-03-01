fbf4a107018e1b99cae927378c204a3aee66b53a
Omkar Rane <omkarrane10@gmail.com>
2020-03-02 01:25:06 +0530

    GraphQL Backend part and mongodb connection done

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id="main">
+      <h1>Omkar's Manga List</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+// Connect to Database
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-vllhb.mongodb.net/${process.env.DB_SCHEMA}?retryWrites=true&w=majority`, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.once('open', () => {
+  console.log("database connected");
+});
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+app.listen(4000, () => {
+  console.log("server started on port 4000");
+});
